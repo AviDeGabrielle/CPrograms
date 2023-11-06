a6984204d0ea7a47fc048ed74b087e9491c280e7
Avi <114779885+AviDeGabrielle@users.noreply.github.com>
2023-11-06 14:11:14 +0000

    Update binarysearch.c
    Program now works with '<' '>' '=' characters as opposed to 0, 1, 2.

--- binarysearch.c
+++ binarysearch.c
@@ -1,50 +1,47 @@
 #include <stdio.h>
 int main() {
-	int lower, upper, guess, smaller, bigger, counter, yes;
-	smaller = 0;
-	bigger = 1;
-	yes = 2;
-	counter = 0;
-	printf("Please insert upper and lower values in the form '[lower] [upper]'\n");
-	scanf("%d %d", &lower, &upper);
-	guess = (lower + upper) / 2;
-	printf("%d\n", guess);
-	scanf("%d", &counter);
-	while (counter != 2) {
-		if (guess < lower){
-			printf("You're cheating!\n");
-			return 0;
-		}
-		if (guess > upper){
-			printf("You're cheating!\n");
-			return 0;
-		}
-		else {
-			switch (counter) {
-				case 0: /*smaller*/
-					upper = (guess - 1);
-					guess = (lower + upper) / 2;
-					printf("%d\n", guess);
-					scanf("%d", &counter);
-					break;
-				case 1: /*bigger*/
-					lower = (guess + 1);
-					guess = (lower + upper) / 2;
-					printf("%d\n", guess);
-					scanf("%d", &counter);
-					break;
-				default:
-					printf("That is not a valid response. Please respond using 0 for smaller, 1 for bigger, and 2 for yes.\n");
-					scanf("%d", &counter);
-					break;
-			}
-		}
-	}
-	while (counter = 2) {
-		printf("yay\n");	
-		return 0;
-	}
+    int lower, upper, guess, c;
+    printf("Please insert upper and lower values in the form '[lower] [upper]'\n");
+    scanf("%d %d", &lower, &upper);
+    guess = (lower + upper) / 2;
+    printf("Please respond with < for less (as in the number than you are thinking of < guess), > for greater, and = for correct!\n\n");
+    printf("%d\n", guess);
+    getchar();
+    c = getchar();
+    while (c != '=') {
+        if (guess < lower){ 
+            printf("You're cheating!\n");
+            return 0;
+        }
+        if (guess > upper){
+            printf("You're cheating!\n");
+            return 0;
+        }
+        switch (c) {
+            case '<': /*smaller*/
+                upper = (guess - 1);
+                guess = (lower + upper) / 2;
+                printf("%d\n", guess);
+                getchar();
+                c = getchar();
+                break;
+            case '>': /*bigger*/
+                lower = (guess + 1);
+                guess = (lower + upper) / 2;
+                printf("%d\n", guess);
+                getchar();
+                c = getchar();
+                break;
+            default:
+                printf("That is not a valid response. Please respond using < for smaller, > for bigger, and = for yes.\n");
+                getchar();
+                c = getchar();
+                break;
+        }
+    }
+    (guess != 69)?printf("Yay!\n"):1;
+    (guess == 69)?printf("Nice\n"):1;    
+    return 0;
 }
 
 // "Guess what number I'm thinking of" Game in C by AviDG
-
